b545209127d8a309db517235473ae65b1d6f6ea9
Adrian De Simone <adesimone@mpublico.gov.ar>
2022-12-14 08:13:13 -0300

    Deployed version

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/drink/:id" element={<CocktailCard />} />
-          <Route path="/random-drink" element="A Random Drink" />
+          {/* <Route path="/random-drink" element="A Random Drink" /> */}
         </Routes>
       </BrowserRouter>
     </div>
